a34d27218bc405af0a2a47d1c76d4cd2510aa0a5
Janaina-MJ <janaina.marostegadejesus@gmail.com>
2024-06-12 13:42:31 +0100

    feat: Add function to pink-arrow button

---
+++ js/global.js
@@ -0,0 +1,6 @@
+// PINK-ARROW FUNCTION: Scroll to Welcome Section using scrollIntoView
+document.getElementById('scroll-button').addEventListener('click', function() {
+  document.getElementById('welcome').scrollIntoView({ behavior: 'smooth' });
+});
+
+
